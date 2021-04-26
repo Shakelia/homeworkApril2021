9a544ec8f3928518bd953d1ee4dc28bed825d7c8
Shakelia Bolden <shakelia_bolden@hotmail.com>
2021-04-26 18:19:15 -0500

    Adding Homework week2

---
+++ ClassActivities/ClassActivities/JSVariblesAndPrimitives.js
@@ -0,0 +1,24 @@
+// create a constant variable (const)
+// create a variable that can be reassigned (let)
+let age = 12;
+console.log (age)
+// create three variables and assign them values of different data types (=)
+let house = "Green"
+let pet = 6
+let happiness = true
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log (typeof house)
+console.log (typeof pet)
+console.log (typeof happiness)
+// change the value referenced by a variable (dynamic typing)
+pet = Charlie
+// print the type of the variable you just changed (typeof)
+console.log (typeof pet)
+// create variables and give them values to complete the sentences that will print
+//hello, my name is __ and I have been learning JavaScript for __ weeks.
+// ? which variables need to be created?
+// ? what type of data needs to go in each variable?
+const name = David;
+let numbofweeks = 5;
+console.log ("Hello, my name is " + "and I have been learning JavaScript for"+ numbofweeks "weeks" )
