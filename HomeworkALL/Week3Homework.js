43c8afdf7cc31817c19df5599952a53f7f8a0da1
Shakelia Bolden <shakelia_bolden@hotmail.com>
2021-05-11 17:48:06 -0500

    HomeworkALL/HomeworkWeek4/index.html

--- HomeworkALL/Week3Homework.js
+++ HomeworkALL/Week3Homework.js
@@ -1,1 +1,44 @@
-let a=0;
+var pizzaToppings = ["pepperoni", "sausage", "onions", "bellpeppers"];
+pizzaLine = []
+
+keyValuePizza ={
+    size: "Medium",
+    crust: "Thick",
+    toppings: "Pepperoni",
+}
+
+function toppingLoop() {
+  availableToppings= []
+  for (i = 0; i < pizzaToppings.length; i++) {
+      availableToppings.push(pizzaToppings[i]);
+}
+  return availableToppings;
+
+function greetCustomer(name){
+console.log(`Welcome ${name} to Shakelia's Pizza Place! We have ${toppingLoop} to go on your pizza!`);
+
+}
+}
+greetCustomer("Shakelia");
+
+function getPizzaOrder (s,c...t){
+  console.log(`one ${s}, ${c}, pizza with ${t} ...`)
+}
+getPizzaOrder("Medium", "Thick Crust", "pepperoni")
+
+function preparePizza (arr) {
+  let pizza = {
+    size: arr.size,
+    crust: arr.crust,
+    toppings: arr.toppings
+}
+  pizzaLine.push(pizza);
+  console.log("Cooking Pizza");
+
+
+
+preparePizza(keyValuePizza);
+  console.log(`Order up here's your ${arr.size} ${arr.crust} pizza with ${arr.top}`)
+}
+
+servePizza(keyValuePizza);
